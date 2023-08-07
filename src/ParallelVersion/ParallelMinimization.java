d28d5d29c27e53d43bfa1b0c04011d1eefac5387
noahgonzy <GNSNOA001@myuct.ac.za>
2023-08-07 16:56:38 +0200

    New Fixes

--- src/ParallelVersion/ParallelMinimization.java
+++ src/ParallelVersion/ParallelMinimization.java
@@ -1,10 +1,11 @@
 package ParallelVersion;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
 
-public class ParallelMinimization extends RecursiveAction {
+public class ParallelMinimization extends RecursiveTask<Integer> {
 	static int rows; //grid size
 	static int columns;
 	static double xmin, xmax, ymin, ymax; //x and y terrain limits
@@ -15,10 +16,11 @@ public class ParallelMinimization extends RecursiveAction {
 	static Search [] searches;		// Array of searches
 	static Random rand = new Random();  //the random number generator
 
-	static int min=Integer.MAX_VALUE;
-    static int local_min=Integer.MAX_VALUE;
-	static int finder =-1;
-	int minsearch,maxsearch;
+	int min=Integer.MAX_VALUE;
+    int local_min=Integer.MAX_VALUE;
+	int finder =-1;
+	int minsearch, maxsearch, leftret, rightret;
+	static int[] nums;
 
 	ParallelMinimization(int minsearchin, int maxsearchin){
 		minsearch = minsearchin;
@@ -26,24 +28,27 @@ public class ParallelMinimization extends RecursiveAction {
 	}
 
 	@Override
-	protected void compute(){
+	protected Integer compute(){
 		if(maxsearch-minsearch <= 8000){
-			for(int i = 0; i < maxsearch-minsearch; i++){
+			for(int i = minsearch; i < maxsearch; i++){
 				local_min=searches[i].find_valleys();
 				if((!searches[i].isStopped())&&(local_min<min)) { //don't look at  those who stopped because hit exisiting path
 					min=local_min;
 					finder=i; //keep track of who found it
 				}
 			}
+			System.out.println(finder);
+			return min;
 		}
 		else{
 			//SORT OUT POINTS VISITED ISSUE
 			int split = (int)((maxsearch+minsearch)/2);
 			ParallelMinimization left = new ParallelMinimization(minsearch,split);
-			ParallelMinimization right = new ParallelMinimization(split+1, maxsearch);
+			ParallelMinimization right = new ParallelMinimization(split, maxsearch);
 			left.fork();
-			right.compute();
-			left.join();
+			rightret = right.compute();
+			leftret = left.join();
+			return Math.min(leftret, rightret);
 		}
 	}
 
@@ -59,14 +64,13 @@ public class ParallelMinimization extends RecursiveAction {
 		endTime=System.currentTimeMillis(); 
 	}
     public static void main(String[] args) {
-		ForkJoinPool threadpool = new ForkJoinPool();
-
-    	
+    	/*
     	if (args.length!=7) {  
     		System.out.println("Incorrect number of command line arguments provided.");   	
     		System.exit(0);
     	}
     	//Read argument values
+		
     	rows =Integer.parseInt( args[0] );
     	columns = Integer.parseInt( args[1] );
     	xmin = Double.parseDouble(args[2] );
@@ -74,17 +78,17 @@ public class ParallelMinimization extends RecursiveAction {
     	ymin = Double.parseDouble(args[4] );
     	ymax = Double.parseDouble(args[5] );
     	searches_density = Double.parseDouble(args[6] );
-
+		*/
 		//TESTING ONLY
-		/*
+		
 		rows = Integer.parseInt("1000");
     	columns = Integer.parseInt("1000");
-    	xmin = Double.parseDouble("500");
-    	xmax = Double.parseDouble("500");
-    	ymin = Double.parseDouble("500");
-    	ymax = Double.parseDouble("500");
+    	xmin = Double.parseDouble("1000");
+    	xmax = Double.parseDouble("-1000");
+    	ymin = Double.parseDouble("1000");
+    	ymax = Double.parseDouble("-1000");
     	searches_density = Double.parseDouble("0.8");
-    	*/
+    	
     	// Initialize 
     	terrain = new TerrainArea(rows, columns, xmin,xmax,ymin,ymax);
     	num_searches = (int)( rows * columns * searches_density );
@@ -93,21 +97,26 @@ public class ParallelMinimization extends RecursiveAction {
     		searches[i]=new Search(i+1, rand.nextInt(rows),rand.nextInt(columns),terrain);
 		}
 
+		//COME HERE
+		nums = new int[searches.length];
+
 		if(DEBUG) {
     		/* Print initial values */
     		System.out.printf("Number searches: %d\n", num_searches);
     		//terrain.print_heights();
     	}
 
+		int newmin;
+		ForkJoinPool threadpool = new ForkJoinPool();
     	//start timer
     	tick();
 
 		ParallelMinimization newprocess = new ParallelMinimization(0, num_searches);
-		threadpool.invoke(newprocess);
+		newmin = threadpool.invoke(newprocess);
 
    		//end timer
    		tock();
-
+		threadpool.close();
 
 		System.out.printf("Run parameters\n");
 		System.out.printf("\t Rows: %d, Columns: %d\n", rows, columns);
@@ -122,7 +131,7 @@ public class ParallelMinimization extends RecursiveAction {
 		System.out.printf("Grid points evaluated: %d  (%2.0f%s)\n",tmp,(tmp/(rows*columns*1.0))*100.0, "%");
 	
 		/* Results*/
-		System.out.printf("Global minimum: %d at x=%.1f y=%.1f\n\n", min, terrain.getXcoord(searches[finder].getPos_row()), terrain.getYcoord(searches[finder].getPos_col()) );
+		System.out.printf("Global minimum: %d at x=%.1f y=%.1f\n\n", newmin, terrain.getXcoord(searches[finder].getPos_row()), terrain.getYcoord(searches[finder].getPos_col()) );
     }
 }
 
